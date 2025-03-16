b99d5d549a58412e4c6d58dbb86fc14fbb69133c
felixig <fiv@gmx.at>
2025-03-16 16:04:52 +0100

    Add files via upload

--- tests/sdoclust_example.py
+++ tests/sdoclust_example.py
@@ -10,7 +10,11 @@ x, y = datasets.make_circles(n_samples=5000, factor=0.3, noise=0.1)
 # SDOclust clustering
 p = sdo.SDOclust().fit_predict(x)
 
+# plotting results
+import matplotlib.pyplot as plt
 fig = plt.figure()
 plt.scatter(x[:,0],x[:,1], s=10, cmap='coolwarm', c=p)
 plt.title('SDOclust clustering', fontsize=14)
 plt.show()
+
+
